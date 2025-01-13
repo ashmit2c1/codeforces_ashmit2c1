821f0e991ef4c8c3e2afdf6e0394a074eab467c8
ashmit2c1 <codeatashmit@gmail.com>
2025-01-13 15:40:45 +0530

    codeforces_ashmit2c1

---
+++ B_Lovely_Palindromes.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define lint long long
+#define forloop(x,y) for(int i=x;i<y;i++)
+#define secondfor(x,y) for(int j=x;j<y;j++)
+#define print(x) cout << x << "\n";
+#define output(arr) for(int i=0;i<arr.size();i++){cout<<arr[i]<<" ";}
+#define asort(v) sort(v.begin(), v.end())
+#define dsort(v) sort(v.begin(), v.end(), greater<int>())
+#define intmax INT_MAX
+#define intmin INT_MIN
+#define lintmax LLONG_MAX
+#define lintmin LLONG_MIN
+#define mp(x,y) make_pair(x,y)
+
+
+string solvefunction(string s){
+    string ans=s;
+    reverse(s.begin(),s.end());
+    ans+=s;
+    return ans;
+}
+
+void solution(){
+    string s;cin >> s;
+    string ans=solvefunction(s);
+    print(ans);
+
+}
+int main(){
+
+    solution();
+}

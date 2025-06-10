5fb500ec589fea0a391aa2b9c7a77108141fa342
ashmit2c1 <codeatashmit@gmail.com>
2025-06-10 23:12:25 +0530

    Submission template

--- 0-template/without_test.cpp
+++ 0-template/without_test.cpp
@@ -13,28 +13,10 @@ using namespace std;
 #define lintmin LLONG_MIN
 #define mp(x,y) make_pair(x,y)
 
-string solveFunction(string s){
-    reverse(s.begin(),s.end());
-    string ans="";
-    forloop(0,s.size()){
-        char character = s[i];
-        if(character=='p'){ans+="q";}
-        if(character=='w'){ans+="w";}
-        if(character=='q'){ans+="p";}
-    }
-    return ans;
-}
+void solution(){
 
-void solution(int test){
-    while(test--){
-        string s;cin >> s;
-        string ans=solveFunction(s);
-        print(ans)
-
-    }
 }
 int main(){
-    int test;
-    cin >> test;
-    solution(test);
+
+    solution();
 }
